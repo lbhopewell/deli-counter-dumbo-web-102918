13808402aecc4141d1a29ddef7a77ac80ea1402f
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 13:48:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,9 @@
 # Write your code here.
 
+
+
+
+
 def take_a_number(katz_deli, name)
   katz_deli << name
   position = katz_deli.length
@@ -15,17 +19,4 @@ def now_serving(katz_deli)
   end
 end
 
-def line(katz_deli)
-  current = []
-  counter = 0 
-  if(katz_deli.length === 0)
-    return "The line is currently empty."
-  else
-    while counter < katz_deli.length
-    current << counter+1 
-    current << ". "
-    current  << katz_deli[counter]
-  end
-  end
-  return "The line is currently: #{current} "
-end
+
