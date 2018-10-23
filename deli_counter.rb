ee06d40f86b2d6f91b54a6361ebb1e7f2c58242d
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 13:53:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,10 @@ def line(katz_deli)
   if(katz_deli.length === 0)
     puts "The line is currently empty."
   else
- 
+    
   end
   end
-  puts "The line is currently: #{current} "
+  
 end
 
 def take_a_number(katz_deli, name)
