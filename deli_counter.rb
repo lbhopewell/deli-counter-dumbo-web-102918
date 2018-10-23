18382e1a228c6144c7da2386705be41edd116ffc
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 13:12:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,15 +3,15 @@
 def take_number(katz_deli, name)
   katz_deli << name
   position = katz_deli.length
-  return "Welcome, #{name}. You are #{position} in line.  
+  return "Welcome, #{name}. You are #{position} in line." 
 end
 
 def now_serving(katz_deli)
   if(katz_deli.length == 0)
-    puts "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!"
   else 
     serving = katz_deli.shift
-    return " Currently serving #{serving}."
+    return "Currently serving #{serving}."
 end
 
 def line (katz_deli)
