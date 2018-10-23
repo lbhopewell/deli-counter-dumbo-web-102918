abca9d70edb5e65f38b2d7128ffa11c5b03a5f23
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 13:29:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,12 +19,13 @@ def line (katz_deli)
   current = ""
   counter = 0 
   if(katz_deli.length == 0)
-  return "The line is currently empty."
+    return "The line is currently empty."
   else
     while counter < katz_deli.length
     current += counter+1 
     current += ". "
     current  += katz_deli[counter]
+  end
   end
   return "The line is currently: #{current} "
 end
