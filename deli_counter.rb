6bd529636e1bba7ad8da321c0411f368fdd91c47
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 15:00:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,10 +10,10 @@ def line(katz_deli)
     current << "#{counter+1}"
     current << ". "
     current << katz_deli[counter]
-    current << " "
+    if(counter<katz_deli-1) current << " "
     counter += 1
     end
-  puts "The line is currently: #{current}"
+  puts "The line is currently: #{current} "
 end
 end
 
