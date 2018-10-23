7d0aa2ea5f1b5ff33da4978c7730a3ccabbe62e0
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 13:46:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 def take_a_number(katz_deli, name)
   katz_deli << name
   position = katz_deli.length
-  puts "Welcome, #{name}. You are #{position} in line." 
+  puts "Welcome, #{name}. You are number #{position} in line." 
 end
 
 def now_serving(katz_deli)
