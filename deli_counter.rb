c8a741deab6c174b60861747211c6015409115ed
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-23 13:26:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,8 +12,19 @@ def now_serving(katz_deli)
   else 
     serving = katz_deli.shift
     return "Currently serving #{serving}."
+  end
 end
 
 def line (katz_deli)
-  puts "The line is "
+  current = ""
+  counter = 0 
+  if(katz_deli.length == 0)
+  return "The line is currently empty."
+  else
+    while counter < katz_deli.length
+    current += counter+1 
+    current += ". "
+    current  += katz_deli[counter]
+  end
+  return "The line is currently: #{current} "
 end
